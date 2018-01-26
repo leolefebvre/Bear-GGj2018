b4e76f3be3d3168f912c50d8c0ee9777a12895d0
troleoleo <leolefebvre.contact@gmail.com>
2018-01-26 23:47:25 +0100

    [New] Add FPS character controller and first terrain

---
+++ Bear-GGC2018/Assets/Script/Controler/MainCharacterControler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterControler : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    [SerializeField] private float _translation = 0f;
+    [SerializeField]  private float _strafe = 0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Lock the mouse on the character
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ManageMovement();
+
+    }
+
+    void ManageMovement ()
+    {
+        _translation = Input.GetAxis("Vertical") * speed;
+        _strafe = Input.GetAxis("Horizontal") * speed;
+
+        _translation *= Time.deltaTime;
+        _strafe *= Time.deltaTime;
+
+        transform.Translate(_strafe, 0f, _translation);
+    }
+}
---
+++ Bear-GGC2018/Assets/Script/Controler/MouseLook.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensivity = 5.0f;
+    public float smoothing = 2.0f;
+
+    public Vector2 yAxisClamp = new Vector2(-90f, 90f);
+
+    private Vector2 _mouseLook;
+    private Vector2 _smoothV;
+
+    
+
+    private GameObject _character;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _character = this.transform.parent.gameObject;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 mousePosition = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        _smoothV.x = Mathf.Lerp(_smoothV.x, mousePosition.x, 1f/smoothing);
+        _smoothV.y = Mathf.Lerp(_smoothV.y, mousePosition.y, 1f / smoothing);
+
+        // Update mouselook posistion
+        _mouseLook += _smoothV;
+        _mouseLook.y = Mathf.Clamp(_mouseLook.y, yAxisClamp.x, yAxisClamp.y);
+
+        transform.localRotation = Quaternion.AngleAxis(-_mouseLook.y, Vector3.right);
+        _character.transform.localRotation = Quaternion.AngleAxis(_mouseLook.x, _character.transform.up);
+    }
+}
